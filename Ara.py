a058a4d33fad851e8a20272beebed0553e8a2b6a
AliReaza Noroozy <a.noroozy95@gmail.com>
2022-08-01 12:35:22 +0430

    last

--- Ara.py
+++ Ara.py
@@ -4,9 +4,7 @@ import Token
 import random
 
 client = discord.Client()
-
 bad_words = ['kir', 'kos', 'kon', 'fuck']
-
 against_bw = ['bi adab nabashim', 'kore khar fosh nade', 'dahaneto ab bemal', 'bia bokhoresh', 'na omid shodam azat -_-']
 
 
@@ -14,21 +12,19 @@ against_bw = ['bi adab nabashim', 'kore khar fosh nade', 'dahaneto ab bemal', 'b
 async def on_ready():
     print('we have logged as {0.user}'.format(client))
 
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     msg = message.content
-
     if msg.startswith("$hello"):
         await message.channel.send("hello darlin !!!")
 
     if any(word in msg for word in bad_words):
         await message.channel.send(random.choice(against_bw))
+
     if msg.startswith("$help"):
       await message.channel.send("This is ARA guid commands:\n\t$hello => bot say hello to you\n\tif you use bad words bot reactioned to your message")
-
 
 client.run(Token.Token())
