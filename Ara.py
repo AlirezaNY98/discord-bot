66d6fee28136bc22ac6aff2ecde8a4909ef0a583
AliReaza Noroozy <a.noroozy95@gmail.com>
2022-08-17 16:21:46 +0430

    add

--- Ara.py
+++ Ara.py
@@ -1,10 +1,20 @@
 from lib2to3.pgen2 import token
 import discord
 import Token
+import urllib.request
+import json
 import random
 
 client = discord.Client()
 good_words = ['afarin', 'ahsant', 'barikala', 'mashala', 'bos', 'damet garm', 'ghorbonet', 'nokaretam', 'mokhlesam' ]
+
+
+def meme():
+    web_url = urllib.request.urlopen("https://api.codebazan.ir/jok/json/")
+    data = web_url.read()
+    meme_JSon = json.loads(data)
+    return meme_JSon["result"]["jok"]
+    
 
 @client.event
 async def on_ready():
@@ -17,10 +27,14 @@ async def on_message(message):
 
     msg = message.content
     if msg.startswith("$help"):
-      await message.channel.send("This is ARA guid commands:\n\t$hello => bot say hello to you\n\tif you use bad words bot reactioned to your message")
+      await message.channel.send("This is ARA guid commands:\n\t$hello => bot say hello to you\n\t \
+        $meme => send joke in channel")
 
     if any(word in msg for word in good_words):
         await message.channel.send("nice...")
+
+    if msg.startswith("$meme"):
+        await message.channel.send(meme())
 
 
 
